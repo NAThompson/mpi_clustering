4d7fcc9b202c4a51f9e7692fa1c49634a91592ae
Nick <nathompson7@protonmail.com>
2016-04-03 16:40:18 +0000

    Add hostname to mpi_hello.c

--- mpi_hello.c
+++ mpi_hello.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 #include <mpi/mpi.h>
 
 int main (int argc, char** argv)
@@ -8,7 +10,10 @@ int main (int argc, char** argv)
     MPI_Init (&argc, &argv);
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);
     MPI_Comm_size (MPI_COMM_WORLD, &size);
-    printf( "Hello world from process %d of %d\n", rank, size );
+    char hostname[150];
+    memset(hostname, 0, 150);
+    gethostname(hostname, 150);
+    printf( "Hello world from process %d of %d on host %s\n", rank, size, hostname );
     MPI_Finalize();
     return 0;
 }
