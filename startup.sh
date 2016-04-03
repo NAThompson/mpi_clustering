808d442d4bfd716bf78215356668b91864e011c6
Nick Thompson <nickadamthompson@gmail.com>
2016-04-03 18:38:12 -0500

    Add SSH configuration

--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y libopenmpi-dev openmpi-bin
+sudo apt-get install -y libopenmpi-dev openmpi-bin git emacs gcc
+echo "StrictHostKeyChecking no" | sudo tee --append /etc/ssh/ssh_config
+echo "HashKnownHosts No" | sudo tee --append ~/.ssh/ssh_config
